f09b405fbe98f2029405ba1356a0057ad619b1a1
Albert Anyika <disiluk15@gmail.com>
2024-08-29 20:05:27 -0700

    python_if_statements

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 def fizzbuzz():
     for i in range(1, 101, 1):
         if (i % 3 == 0) and (i % 5 == 0):
-            print("{}, ".format("FizzBuzz"), end="")
+            print("{} ".format("FizzBuzz"), end="")
         elif (i % 5 == 0) and (i != 100):
-            print("{}, ".format("Buzz"), end="")
+            print("{} ".format("Buzz"), end="")
         elif i % 3 == 0:
-            print("{}, ".format("Fizz"), end="")
+            print("{} ".format("Fizz"), end="")
         elif i == 100:
             print("{}".format("Buzz"), end="")
         else:
-            print("{}, ".format(i), end="")
+            print("{} ".format(i), end="")
