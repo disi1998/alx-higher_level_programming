13e66a59bd3bae08ed50c1e740c2d40dea9a75a2
Albert Anyika <disiluk15@gmail.com>
2024-09-06 15:58:44 -0700

    python_imort_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print(f"{len(argv) - 1} arguments.")
+elif len(argv) > 2:
+    print(f"{len(argv) - 1} arguments:")
+    for i in range(1, len(argv), 1):
+        print(f"{i}: {argv[i]}")
+else:
+    print(f"{len(argv) - 1} argument:")
+    print(f"1: {argv[len(argv) - 1]}")
