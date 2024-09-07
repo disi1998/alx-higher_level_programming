460b30c2fdd4a13c142025aba534675c57ba42ac
Albert Anyika <disiluk15@gmail.com>
2024-09-06 18:12:56 -0700

    python_imort_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) == 1:
-    print(f"{len(argv) - 1} arguments.")
-elif len(argv) > 2:
-    print(f"{len(argv) - 1} arguments:")
-    for i in range(1, len(argv), 1):
-        print(f"{i}: {argv[i]}")
-else:
-    print(f"{len(argv) - 1} argument:")
-    print(f"1: {argv[len(argv) - 1]}")
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print(f"{len(argv) - 1} arguments.")
+    elif len(argv) > 2:
+        print(f"{len(argv) - 1} arguments:")
+        for i in range(1, len(argv), 1):
+            print(f"{i}: {argv[i]}")
+    else:
+        print(f"{len(argv) - 1} argument:")
+        print(f"1: {argv[len(argv) - 1]}")
