b1422c1f8e8cf8aca7e5aafb7c881a338682e1c4
Albert Anyika <disiluk15@gmail.com>
2024-09-09 16:50:23 -0700

    python_imort_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,24 @@
 if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import add, sub, mul, div
-    if (argv[2] != "-") and (argv[2] != "*") and (argv[2] != "+") and (argv[2] != "/"):
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    elif len(argv) == 4:
+    if len(argv) == 4:
         a = int(argv[1])
         b = int(argv[3])
         if argv[2] == "+":
             print(f"{a} {argv[2]} {b} = {add(a, b)}")
+            exit(0)
         elif argv[2] == "-":
             print(f"{a} {argv[2]} {b} = {sub(a, b)}")
+            exit(0)
         elif argv[2] == "*":
             print(f"{a} {argv[2]} {b} = {mul(a, b)}")
+            exit(0)
         elif argv[2] == "/":
             print(f"{a} {argv[2]} {b} = {div(a, b)}")
-        exit(0)
+            exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
     elif len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
